a0e78db775615c5ee894a5e55e2b0793dedd34d8
Bruno Caneo <brunocaneo3@gmail.com>
2021-11-23 13:26:00 -0300

    Adding Home and Subscribe button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscibeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,6 +9,22 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> World
+          </h1>
+          <p>
+            Get acess to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Avatar" />
+      </main>
     </>
   );
 }
