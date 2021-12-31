a3789a66742a3a9f06ea06d0e775fad51d16f1d9
Bruno Caneo <brunocaneo3@gmail.com>
2021-12-31 12:12:36 -0300

    Adding Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import { stripe } from "../services/stripe";
 
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1K8WZBDIQdBpa4XIzpXIK3UC");
 
   const product = {
@@ -56,5 +56,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
