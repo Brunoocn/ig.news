443933705016ce1bbe6dffcd4fce9b6f78f0cddd
Bruno Caneo <brunocaneo3@gmail.com>
2022-03-23 02:17:19 -0300

    Adding test to the home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import { SubscribeButton } from "../components/SubscibeButton";
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -32,7 +32,7 @@ export default function Home({ product }: HomeProps) {
             Get acess to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Avatar" />
